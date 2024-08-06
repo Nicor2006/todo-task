9b4f096f3b872ad5fbf6497428cad6351adf3e89
NicolasRivera <nicolasrpv02@gmail.com>
2024-08-05 20:47:02 -0500

    Task schema, Create Task

--- api.js
+++ api.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
 
-const db = (query);
 
 let environment = null;
 
@@ -20,19 +19,43 @@ environment = {
 };
 
 var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
-
+const db = (query);
+var TaskModel = require('./task_schema');
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(db, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 }, function (error) {
     if (error) {
         console.log("Error!" + error);
     } else {
         console.log("Se ha conectado con la base de datos exitosamente aaaaaaaaaaaaaaaa");
     }
+});
+
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+    var newTask = new TaskModel(task);
+
+    newTask.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send("OK\n");
+        }
+    });
 });
 
 module.exports = router;
